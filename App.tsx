79596a83ab85463306e75b80ea912bf895a4295f
Miguel Ramos <return.infox@gmail.com>
2024-11-07 16:28:53 -0300

    aplicado lo que es proveedor

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ItemProvider } from "./src/contexts/ItemContext";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ItemProvider>
+      <PaperProvider>
+        
+      </PaperProvider>
+    </ItemProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
