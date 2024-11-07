fcaddd19e6e6fbff424cefd04d4e334a3d0042aa
Miguel Ramos <return.infox@gmail.com>
2024-11-07 16:55:34 -0300

    app navegacion

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { ItemProvider } from "./src/contexts/ItemContext";
 import { Provider as PaperProvider } from "react-native-paper";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
     <ItemProvider>
       <PaperProvider>
-        
+        <AppNavigator/>
       </PaperProvider>
     </ItemProvider>
   );
