16e601dbbff854ee57360af223ee27c2c7c9fbf1
Miguel Ramos <return.infox@gmail.com>
2024-11-12 14:40:15 -0300

    hooks para el contexto

--- src/hooks/useItemContext.ts
+++ src/hooks/useItemContext.ts
@@ -2,5 +2,10 @@ import { useContext } from "react";
 import { ItemContext } from "../contexts/ItemContext";
 
 export const useItemContext = () => {
-  
+  const context = useContext(ItemContext);
+  if(!context) {
+    throw new Error('useItemContext debe usar dentro del itemprovider');
+  }
+
+  return context;
 }
