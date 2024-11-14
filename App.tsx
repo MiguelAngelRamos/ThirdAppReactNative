e97a6474d0e9ad647254759b84ee9442aed4a8d0
Miguel Ramos <return.infox@gmail.com>
2024-11-14 14:25:55 -0300

    creacion de item en firebase

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { ItemProvider } from "./src/contexts/ItemContext";
 import { Provider as PaperProvider } from "react-native-paper";
 import AppNavigator from "./src/navigation/AppNavigator";
-
+import './src/firebaseConfig';
 
 export default function App() {
   return (
--- src/contexts/ItemContext.tsx
+++ src/contexts/ItemContext.tsx
@@ -1,14 +1,18 @@
 import { createContext, ReactNode, useState } from "react";
+import { fireStore } from "../firebaseConfig";
+import { doc, collection, setDoc } from 'firebase/firestore';
+import { Alert } from "react-native";
 
 interface Item {
   id: string;
   name: string;
   description: string;
+  imageUrl?: string | null;
 }
 // Define la interfaz para las propiedades del contexto de item
 interface ItemContextProps {
   items: Item[];
-  addItem: (item: Item) => void;
+  addItem: (item: Omit<Item, 'id'>, image: string | null) => Promise<void>;
   updateItem: (item: Item) => void;
   deleteItem: (id: string) => void;
 }
@@ -19,8 +23,28 @@ export const ItemContext = createContext<ItemContextProps|undefined>(undefined);
 export const ItemProvider = ({ children } : { children: ReactNode}) => {
   // Estado de la lista de los items
   const [items, setItems] = useState<Item[]>([]);
+  
   // Función para crear
-  const addItem = (item: Item) => setItems((prevItems) => [...prevItems, item]);
+  const addItem = async (item: Omit<Item, 'id'>, image: string | null) => {
+    try {
+      let imageUrl = '';
+
+      // Generamos el documento que enviar a la base de datos
+      const newItemRef = doc(collection(fireStore, 'items'));
+      const newItem = {
+        id: newItemRef.id,
+        ...item,
+        imageUrl
+      }
+      await setDoc(newItemRef, newItem);
+      // el codigo siguiente
+      setItems((prevItems) => [...prevItems, newItem]);
+    } catch (error) {
+      Alert.alert("Error", "No se puedo crear el item"); 
+    }
+  };
+
+
   // Funcion para actualizar los items de la lista
   const updateItem = (updatedItem: Item) =>
     setItems((prevItems) => 
